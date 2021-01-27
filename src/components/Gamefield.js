894daed8cdc58ea2b59617e185a24a99bf19a229
Tom <tchao770@gmail.com>
2021-01-27 15:42:52 -0800

    Cards are randomized when clicked

--- src/components/Gamefield.js
+++ src/components/Gamefield.js
@@ -1,19 +1,37 @@
-import { useState, useRef, useReducer, Fragment } from 'react';
+import { useState, useRef, Fragment } from 'react';
 import Card from "./Card.js"
+
+function shuffle(arr) {
+    var currentIndex = arr.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = arr[currentIndex];
+        arr[currentIndex] = arr[randomIndex];
+        arr[randomIndex] = temporaryValue;
+    }
+
+    return arr;
+}
 
 export default function GameField(props) {
     const [score, setScore] = useState(0);
     const highscore = useRef(0);
-
-    const cardName =
-        [
-            "Re:Zero", "Sword Art Online", "Overlord",
-            "Konosuba", "Shield Hero", "That Time I Got Reincarnated as a Slime",
-            "Devil's a Parttimer", "No Game No Life", "Mondaiji Tachi"
-        ];
+    const [cardName, setCardName] = useState([
+        "Re:Zero", "Sword Art Online", "Overlord",
+        "Konosuba", "Shield Hero", "That Time I Got Reincarnated as a Slime",
+        "Devil's a Parttimer", "No Game No Life", "Mondaiji Tachi"
+    ]);
 
     const onClick = () => {
-        setScore(score + 1)
+        setScore(score + 1);
+        shuffle(cardName);
     }
 
     return (
@@ -27,7 +45,7 @@ export default function GameField(props) {
                 {
                     cardName.map((card) => {
                         return (
-                            <Card title={card} key={card} onClick={onClick}/>
+                            <Card title={card} onClick={onClick} />
                         );
 
                     })
