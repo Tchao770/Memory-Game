a39b4ca543dd5c0635deb17770aa92a95b4c5085
Tom <tchao770@gmail.com>
2021-01-27 15:08:00 -0800

    Additional detailing and layout of game

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import {useState, useRef, useReducer} from 'react';
 export default function Card(props){
     return(
         <div className="Card">
-            <p>Digimon</p>
+            <p>{props.title}</p>
         </div>
     )
 }
--- src/components/Gamefield.js
+++ src/components/Gamefield.js
@@ -2,14 +2,26 @@ import { useState, useRef, useReducer } from 'react';
 import Card from "./Card.js"
 
 export default function GameField(props) {
-    
+
+    const cardName =
+        [
+            "Re:Zero", "Sword Art Online", "Overlord",
+            "Konosuba", "Shield Hero", "That Time I Got Reincarnated as a Slime",
+            "Devil's a Parttimer", "No Game No Life", "Mondaiji Tachi"
+        ]
+
     return (
         <div>
-            <h1 className="heading">Memory game</h1>
+            <h1 className="heading">Memory Game: Name That Isekai</h1>
             <div className="fieldContainer">
-                <Card />
-                <Card />
-                <Card />
+                {
+                    cardName.map((card) => {
+                        return (
+                            <Card title={card}/>
+                        );
+
+                    })
+                }
             </div>
         </div>
     )
