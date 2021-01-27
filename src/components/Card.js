af70c61900b3382949228764aa4065bd6a7ae7c0
Tom <tchao770@gmail.com>
2021-01-27 15:29:34 -0800

    Added score counter upon clicking a card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import {useState, useRef, useReducer} from 'react';
 
 export default function Card(props){
     return(
-        <div className="Card">
+        <div className="Card" onClick={props.onClick}>
             <p>{props.title}</p>
         </div>
     )
--- src/components/Gamefield.js
+++ src/components/Gamefield.js
@@ -1,28 +1,38 @@
-import { useState, useRef, useReducer } from 'react';
+import { useState, useRef, useReducer, Fragment } from 'react';
 import Card from "./Card.js"
 
 export default function GameField(props) {
+    const [score, setScore] = useState(0);
+    const highscore = useRef(0);
 
     const cardName =
         [
             "Re:Zero", "Sword Art Online", "Overlord",
             "Konosuba", "Shield Hero", "That Time I Got Reincarnated as a Slime",
             "Devil's a Parttimer", "No Game No Life", "Mondaiji Tachi"
-        ]
+        ];
+
+    const onClick = () => {
+        setScore(score + 1)
+    }
 
     return (
-        <div>
+        <Fragment>
             <h1 className="heading">Memory Game: Name That Isekai</h1>
+            <div className="scoreCounter">
+                <p>Score: {score}</p>
+                <p>Highscore: {highscore.current}</p>
+            </div>
             <div className="fieldContainer">
                 {
                     cardName.map((card) => {
                         return (
-                            <Card title={card}/>
+                            <Card title={card} key={card} onClick={onClick}/>
                         );
 
                     })
                 }
             </div>
-        </div>
+        </Fragment>
     )
 }
